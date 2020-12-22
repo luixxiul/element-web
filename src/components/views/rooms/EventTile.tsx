ca4fa33dda5812b259c4391171358a50beb812fd
su-ex <codeworks@supercable.onl>
2020-12-22 19:16:28 +0100

    No red messages in 1:1 chats Close SchildiChat/schildichat-desktop#13

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -685,6 +685,12 @@ export class UnwrappedEventTile extends React.Component<EventTileProps, IState> 
             return false;
         }
 
+        // don't show highlights in 1:1 rooms
+        const room = cli.getRoom(this.props.mxEvent.getRoomId());
+        if (room && room.currentState.getJoinedMemberCount() === 2) {
+            return false;
+        }
+
         return !!(actions?.tweaks.highlight || previousActions?.tweaks.highlight);
     }
 
