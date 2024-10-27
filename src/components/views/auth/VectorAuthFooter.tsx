4290ab5db91968c183402ef260f9b20b2670bcc3
SpiritCroc <dev@spiritcroc.de>
2024-10-27 20:07:30 +0100

    Schildify login footer

--- src/components/views/auth/VectorAuthFooter.tsx
+++ src/components/views/auth/VectorAuthFooter.tsx
@@ -14,9 +14,10 @@ import { _t } from "../../../languageHandler";
 const VectorAuthFooter = (): ReactElement => {
     const brandingConfig = SdkConfig.getObject("branding");
     const links = brandingConfig?.get("auth_footer_links") ?? [
-        { text: "Blog", url: "https://element.io/blog" },
-        { text: "Twitter", url: "https://twitter.com/element_hq" },
-        { text: "GitHub", url: "https://github.com/element-hq/element-web" },
+        //{ text: "Blog", url: "https://element.io/blog" },
+        //{ text: "Twitter", url: "https://twitter.com/element_hq" },
+        { text: "About", url: "https://schildi.chat" },
+        { text: "GitHub", url: "https://github.com/schildichat/schildichat-desktop" },
     ];
 
     const authFooterLinks: JSX.Element[] = [];
